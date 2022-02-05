5ad11564182a0bba2a9e472a85225dc08179543e
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2022-02-05 01:30:24 -0500

    Implement color constant and macros

--- src/utils.rs
+++ src/utils.rs
@@ -8,10 +8,31 @@ pub struct Color {
     pub blue: u8
 }
 
+#[macro_export]
+macro_rules! color {
+    ($r:expr,$g:expr,$b:expr) => {
+        Color {
+            red: $r,
+            green: $g,
+            blue: $b
+        }
+    };
+}
+
 impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{} {} {}", self.red, self.green, self.blue)
     }
+}
+
+pub mod color_constants {
+    use crate::utils::Color;
+
+    pub const BLACK: Color = color!(0, 0, 0);
+    pub const RED: Color = color!(255, 0, 0);
+    pub const GREEN: Color = color!(0, 255, 0);
+    pub const BLUE: Color = color!(0, 0, 255);
+    pub const WHITE: Color = color!(255, 255, 255);
 }
 
 #[derive(Clone, Debug)]
