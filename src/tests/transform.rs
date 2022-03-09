745a5a288a890a14c1c008c64ce2a2e655cadac5
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2022-03-09 03:14:18 -0500

    Add clock for gallery

---
+++ src/tests/transform.rs
@@ -0,0 +1,12 @@
+use std::fs::{self, File};
+
+use crate::Parser;
+
+#[test]
+fn clock() {
+    fs::create_dir_all("clockframes").expect("Clock directory creation failed");
+    for i in 0..60 {
+        let mut p: Parser = Default::default();
+        p.parse(File::open(format!("src/tests/clockscripts/clock{}", i).as_str()).expect("File read failed"))
+    }
+}
