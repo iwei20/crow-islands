78ff2c5f9d8f68f93a6de18b7ba56274e4eb34f4
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2022-03-09 03:19:30 -0500

    Zero pad files for ffmpeg

--- src/tests/transform.py
+++ src/tests/transform.py
@@ -3,7 +3,7 @@ HOUR_LENGTH = 125
 MARK_LENGTH = 25
 
 for i in range(60):
-    with open(f"clockscripts/clock{i}", "w") as fout:
+    with open(f"clockscripts/clock{str(i).zfill(2)}", "w") as fout:
         fout.write("# Minute hand\n")
         fout.write(f"line 0 0 0 0 {MINUTE_LENGTH} 0\n")
         fout.write(f"rotate z {i * -6}\n")
@@ -23,4 +23,4 @@ for i in range(60):
         fout.write("ident\n")
         fout.write("move 250 250 0\n")
         fout.write("apply\n")
-        fout.write(f"save clockframes/clock{i}.png\n")
+        fout.write(f"save clockframes/clock{str(i).zfill(2)}.png\n")
--- src/tests/transform.rs
+++ src/tests/transform.rs
@@ -7,6 +7,6 @@ fn clock() {
     fs::create_dir_all("clockframes").expect("Clock directory creation failed");
     for i in 0..60 {
         let mut p: Parser = Default::default();
-        p.parse(File::open(format!("src/tests/clockscripts/clock{}", i).as_str()).expect("File read failed"))
+        p.parse(File::open(format!("src/tests/clockscripts/clock{:0>2}", i).as_str()).expect("File read failed"))
     }
 }
