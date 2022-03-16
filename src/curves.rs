759b6da298f3bcd01a80dd96a8d9021d69dc5bed
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2022-03-15 23:52:22 -0400

    Implement points generator

--- src/curves.rs
+++ src/curves.rs
@@ -8,6 +8,12 @@ pub trait Parametric {
     fn f(&self, t: f64) -> (f64, f64, f64) {
         (self.x(t), self.y(t), self.z(t))
     }
+
+    fn points(&self, n: usize) -> Vec<(f64, f64, f64)> {
+        (0..n)
+            .map(|i| -> (f64, f64, f64) {self.f(i as f64 / (n - 1) as f64)})
+            .collect()
+    }
 }
 
 pub struct Circle {
