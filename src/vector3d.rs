0f4d3a541520064ebc6fd768e987a164a7bb677e
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2022-04-03 22:38:01 -0400

    Implement vector froms

--- src/vector3d.rs
+++ src/vector3d.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Copy, Debug)]
 pub struct Vector3D {
     pub x: f64,
     pub y: f64, 
@@ -10,6 +11,22 @@ impl Vector3D {
             x,
             y,
             z
+        }
+    }
+
+    pub fn from_point(p: (f64, f64, f64)) -> Self {
+        Self {
+            x: p.0,
+            y: p.1,
+            z: p.2
+        }
+    }
+
+    pub fn from_points(p0: (f64, f64, f64), p1: (f64, f64, f64)) -> Self {
+        Self {
+            x: p1.0 - p0.0,
+            y: p1.1 - p0.1,
+            z: p1.2 - p0.2
         }
     }
 
