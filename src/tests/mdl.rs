03fbecbf0b0cf9aef5ca8915039b137d9f1fa941
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2022-05-23 00:29:59 -0400

    Create solenoid rehash for gallery

---
+++ src/tests/mdl.rs
@@ -0,0 +1,8 @@
+use std::fs::File;
+
+use crate::MDLParser;
+#[test]
+fn main() {
+    let mut p: MDLParser = Default::default();
+    p.parse_file(File::open("src/tests/solenoid.mdl").expect("File open failed")).expect("Program parse failed");
+}
